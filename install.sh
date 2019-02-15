d07033d4e69191f7077f77c987a4391e01b956be
robbie <tidus081@naver.com>
2019-02-15 13:43:33 -0500

    cp

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get install python3-pip python3-dev
 fi
 
+pip3 install virtualenv
+virtualenv venv 
+source venv/bin/activate
+
 # Install requirement python libraries for service_base
 pip3 install -r requirements.txt
 cp utils/ flask_server/
