0113bc28c59e601fe73a9e12ab37db3bfe85aacb
robbie <tidus081@naver.com>
2019-02-18 17:42:34 -0500

    requirement

--- install.sh
+++ install.sh
@@ -6,13 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get install python3-pip python3-dev
 fi
 
-pip3 install virtualenv
-virtualenv venv 
-source venv/bin/activate
-
 # Install requirement python libraries for service_base
 pip3 install -r requirements.txt
-cp utils/ flask_server/
-cp utils/ model_server/
-cp utils/ store_server/
-cp utils/ vec_server/
+cp -r utils/* flask_server/
+cp -r utils/* model_server/
+cp -r utils/* store_server/
+cp -r utils/* vec_server/
